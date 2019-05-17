5bae0515c3063cfd6970818fbd572fae7fb23a32
Alex Puschinsky <alexpoo@gmail.com>
2019-05-17 23:28:06 +0300

    Use multiple cursors to make folding faster

--- extension.js
+++ extension.js
@@ -1,9 +1,9 @@
-var vscode = require('vscode');
+const vscode = require('vscode');
 
 const METHODS_TO_FOLD_CONFIG_NAME = 'methodsToFold';
 
 function activate(context) {
-  var disposable = vscode.commands.registerCommand('foldit.fold', foldCommand);
+  const disposable = vscode.commands.registerCommand('foldit.fold', foldCommand);
   context.subscriptions.push(disposable);
 }
 
@@ -13,49 +13,49 @@ async function foldCommand() {
     .get(METHODS_TO_FOLD_CONFIG_NAME);
 
   const editor = vscode.window.activeTextEditor;
+  const { document } = editor;
 
-  const initialPosition = editor.selection.active.line;
+  const linesToFold = findLineNumbersOfMethods(document, methodsToFold);
 
-  for (const m of methodsToFold) {
-    await foldByMethodName(editor, m)
-  }
-
-  moveCursorTo(editor, initialPosition);
+  await foldLines(editor, linesToFold);
 }
 
-async function foldByMethodName(editor, methodName) {
-  const itLines = findLineNumbersByString(editor.document, methodName);
-
-  for (const l of itLines) {
-    await foldLine(editor, l);
-  }
+function findLineNumbersOfMethods(document, methodsNames) {
+  return methodsNames
+    .map(methodName => findLineNumbersOfMethod(document, methodName))
+    .reduce((out, cur) => [...out, ...cur], []);
 }
 
-async function foldLine(editor, lineNum) {
-  moveCursorTo(editor, lineNum);
-  await vscode.commands.executeCommand('editor.fold');
-}
-
-function findLineNumbersByString(doc, specMethodName) {
+function findLineNumbersOfMethod(doc, methodName) {
   const text = doc.getText();
-  const itRegEx = new RegExp(`${specMethodName}\\s*\\(`);
+  const methodNameRegEx = new RegExp(`${methodName}\\s*\\(`);
 
   return text
     .split(/\r?\n/)
     .map((line, i) => [line, i])
-    .filter(([line, i]) => itRegEx.test(line))
-    .map(([line, i]) => i);
+    .filter(([line]) => methodNameRegEx.test(line))
+    .map(([_line, i]) => i);
 }
 
-function moveCursorTo(editor, lineNum) {
+async function foldLines(editor, linesToFold) {
+  const initialPosition = editor.selection.active.line;
+  moveCursorTo(editor, linesToFold);
+  await vscode.commands.executeCommand('editor.fold');
+  moveCursorTo(editor, [initialPosition]);
+}
+
+function moveCursorTo(editor, lineNumbers) {
   const position = editor.selection.active;
 
-  var newPosition = position.with(lineNum, 0);
-  var newSelection = new vscode.Selection(newPosition, newPosition);
-  editor.selection = newSelection;
+  const newSelections = lineNumbers.map((lineNumber) => {
+    const newPosition = position.with(lineNumber, 0);
+    return new vscode.Selection(newPosition, newPosition);
+  });
+
+  // eslint-disable-next-line no-param-reassign
+  editor.selections = newSelections;
 }
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = { activate, deactivate };
